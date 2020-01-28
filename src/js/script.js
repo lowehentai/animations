f094dc8f78928a276531989a42707e54cfdbc0fa
lowehentai <58338966+lowehentai@users.noreply.github.com>
2020-01-28 17:51:14 +0300

    new Version animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,40 @@
 new CircleType(document.getElementById('demo1'));
 
-// var a = 1,
-//     timer = setInterval(function () {
-//         if (a == 1) {
-//             document.getElementById('demo1').classList.add('container_red');
-//             a++;
-//         }
-//     }, 1000);
 
-let b = 1,
-    timerBeta = setInterval(function () {
-        if (b == 1) {
-            document.getElementById('inner').classList.add('clock-inner_red');
-            b++;
-        }
-    }, 1200);
+var a = 1,
+    b = 1,
+    timers,
 
-let a = 1,
-    timerAlpha = setInterval(function () {
+    timer = setInterval(function () {
         if (a == 1) {
-            document.getElementById('demo1').classList.remove('container_red');
-            document.getElementById('inner').classList.remove('clock-inner_red');
-
-            document.getElementById('demo1').classList.add('container_green');
-            document.getElementById('inner').classList.add('clock-inner_green');
-
-            setInterval(function () {
-                document.getElementById('inner').classList.remove('clock-inner_green');
-                document.getElementById('demo1').classList.remove('container_green');
-            }, 1000);
+            var duration = 10;
+            $("span").each(function (index) {
+                $(this).delay(duration * index).fadeOut(duration).css("display", "block");
+            });
             a++;
+
+            timers = setInterval(function () {
+                if (b == 1) {
+                    var duration = 10;
+                    $("span").each(function (index) {
+                        $(this).delay(duration * index).fadeIn(duration).css("display", "none");
+                    });
+                    b++;
+                    setInterval(function () {
+                        document.getElementById('loader').classList.remove('loader_hidden');
+                        document.getElementById('demo1').classList.add('container_animation');
+                    }, 700);
+                    setInterval(function () {
+                        document.getElementById('inner').classList.remove('clock-inner_hidden');
+                    }, 8000);
+                }
+            }, 600);
         }
-    }, 6000);
+    }, 600);
+
+
+// document.getElementById('demo1').fadeIn();
+// document.getElementById('inner').classList.remove('clock-inner_red');
+
+// document.getElementById('demo1').classList.add('container_green');
+// document.getElementById('inner').classList.add('clock-inner_green');
